ffaf035dbb8e2d7f83eb5f25fe9bf09f3bc79981
LAKSHAY BHARADWAJ <lbharadwaj53@gmail.com>
2023-01-11 00:14:24 +0530

    side container added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -44,7 +44,7 @@ const Header = () => {
         });
     }
     return (
-        <header className='fixed z-50 w-screen p-3 px-4 md:px-16'>
+        <header className='fixed z-50 w-screen p-3 px-4 md:px-16 bg-primary'>
             {/* desktop & Tablet */}
             <div className='hidden md:flex w-full h-full justify-between'>
                 <Link to={"/"} className='flex items-center gap-2'>
@@ -84,7 +84,14 @@ const Header = () => {
                                     {
                                         user && user.email === "lbharadwaj53@gmail.com" && (
                                             <Link to={'/createItem'}>
-                                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'>New Item <MdAdd /></p></Link>
+                                                <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+
+                                                    onClick={() => setisMenu(false)}
+
+                                                >
+                                                    New Item <MdAdd />
+                                                </p>
+                                            </Link>
                                         )
                                     }
 
@@ -134,10 +141,18 @@ const Header = () => {
                                     )
                                 }
                                 <ul className='flex flex-col'>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Home</li>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Menu</li>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>About Us</li>
-                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>Service</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                                        onClick={() => setisMenu(false)}
+                                    >Home</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                                        onClick={() => setisMenu(false)}
+                                    >Menu</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                                        onClick={() => setisMenu(false)}
+                                    >About Us</li>
+                                    <li className='text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                                        onClick={() => setisMenu(false)}
+                                    >Service</li>
                                 </ul>
                                 <p className='m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base' onClick={logout}>Logout <MdLogout /></p>
                             </motion.div>
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Delivery from "../img/delivery.png"
+import HeroBg from "../img/heroBg.png"
+import I1 from "../img/i1.png"
+import { heroData } from '../utils/data'
+
+
 
 const HomeContainer = () => {
     return (
+
         <section className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full ' id='home'>
             <div className='py-2 flex-1 flex flex-col items-start  justify-center gap-6'>
                 <div className='flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full'>
@@ -17,7 +23,21 @@ const HomeContainer = () => {
                 <p className='text-base text-textColor text-center md:text-left md:w-[80%]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae architecto fugit, consectetur incidunt labore rerum expedita fuga sequi ipsum vel ipsa? Saepe consectetur exercitationem reprehenderit, provident nobis magni distinctio. Sed.</p>
                 <button type='button' className='bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'> Order Now</button>
             </div>
-            <div className='py-2 bg-blue-400 flex-1'>  </div>
+            <div className='py-2 flex-1 flex items-center relative '>
+                <img src={HeroBg} className=" ml-auto h-420 w-full lg:w-auto lg:h-650 " alt="hero-bg" />
+
+                <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-4 gap-4 flex-wrap drop-shadow-lg'>
+                    {heroData && heroData.map(n => (
+                        <div key={n.id}
+                            className='lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center'>
+                            <img src={n.imageSrc} className="w-20 lg:w-40 -mt-10 lg:-mt-20" alt={n.name} />
+                            <p className='text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4'>{n.name}</p>
+                            <p className='text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3'>{n.decp} </p>
+                            <p className='text-sm font-semibold text-headingColor'><span className='text-xs text-red-600'>$</span>{n.price}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </section>
     )
 }
